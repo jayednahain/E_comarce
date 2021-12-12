d9f74ad190c8f34f020b8e9fc6f448ae9ab56b2a
jayednahian <jayednahian@yahoo.com>
2021-12-12 19:57:38 +0600

    update circular import error on cart manager

---
+++ shopping_Cart/custom_cart_manager.py
@@ -0,0 +1,31 @@
+from django.db import models
+# from shopping_Cart.models import Cart
+
+import shopping_Cart.models
+
+
+class CartManager(models.Manager):
+   def new_or_get(self,request):
+      cart_id = request.session.get("cart_id", None)
+      qs = self.get_queryset().filter(id=cart_id)
+      if qs.count() == 1:
+         #new
+         new_obj= False
+         print('card ID exists')
+         cart_obj = qs.first()
+         if request.user.is_authenticated and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
+      else:
+         cart_obj = models.Cart.objects.new(user=request.user)
+         #new
+         new_obj =True
+         request.session['cart_id'] = cart_obj.id
+      return cart_obj,new_obj
+   def new_cart(self,user=None):
+      user_obj = None
+      if user is not None:
+         if user.is_authenticated:
+            user_obj=user
+      return self.model.objects.create(user=user_obj)
+
--- shopping_Cart/models.py
+++ shopping_Cart/models.py
@@ -1,10 +1,37 @@
 from django.conf import settings
 from django.db import models
 from product.models import Porduct
-from shopping_Cart.custom_manager import CartManager
+from shopping_Cart.custom_cart_manager import CartManager
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
+
+
+
+# class CartManager(models.Manager):
+#    def new_or_get(self,request):
+#       cart_id = request.session.get("cart_id", None)
+#       qs = self.get_queryset().filter(id=cart_id)
+#       if qs.count() == 1:
+#          #new
+#          new_obj= False
+#          print('card ID exists')
+#          cart_obj = qs.first()
+#          if request.user.is_authenticated and cart_obj.user is None:
+#             cart_obj.user = request.user
+#             cart_obj.save()
+#       else:
+#          cart_obj = Cart.objects.new_cart(user=request.user)
+#          #new
+#          new_obj =True
+#          request.session['cart_id'] = cart_obj.id
+#       return cart_obj,new_obj
+#    def new_cart(self,user=None):
+#       user_obj = None
+#       if user is not None:
+#          if user.is_authenticated:
+#             user_obj=user
+#       return self.model.objects.create(user=user_obj)
 
 class Cart(models.Model):
    user         = models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
@@ -12,9 +39,7 @@ class Cart(models.Model):
    total        = models.DecimalField(default=0.00,max_digits=100,decimal_places=2)
    updated      = models.DateTimeField(auto_now=True)
    created_time = models.DateTimeField(auto_now_add=True)
-
    objects      = CartManager()
-
 
    def __str__(self):
       return str(self.id)
